de01cc41db2db869f57750051480150871402177
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2023-02-12 11:56:14 +0700

    push code

---
+++ app.js
@@ -0,0 +1,14 @@
+let show = document.getElementById("import-search").value;
+function hisSearch() {
+  document.getElementById("show-history_text").innerHTML = show;
+}
+$('#input').keypress(function (event) {
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if (keycode == 13) {
+    alert('anh yêu em');
+  }
+});
+// show cart
+function showmodalCart() {
+  window.location = "http://127.0.0.1:5500/html/cart.html"
+}
